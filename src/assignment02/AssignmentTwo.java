9cc8e8b67c97b22fbc4de99d006f6f85ba09d39f
Manoj Kota <kota.manoj8@gmail.com>
2022-02-21 21:36:01 -0600

    deleted

--- src/assignment02/AssignmentTwo.java
+++ src/assignment02/AssignmentTwo.java
@@ -1,9 +1,13 @@
 package assignment02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 /**
  * 
  * @author Manoj Kota
@@ -15,19 +19,11 @@ public class AssignmentTwo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Problem01 obj1=new Problem01();
-		Problem02 obj2=new Problem02();
-		Problem03 obj3=new Problem03();
-		Problem04 obj4=new Problem04();
-		Problem05 obj5=new Problem05();
-		Problem06 obj6=new Problem06();
-
-
 
 
 	Scanner sc=new Scanner(System.in);
 	 System.out.println("Problem -1");
-	 System.out.print("Enter size of String:- ");
+	 System.out.print("Enter size of String Array:- ");
 		int n = sc.nextInt();
 		String[] t = new String[n];
 
@@ -40,7 +36,7 @@ public class AssignmentTwo {
 
 		}
 
-obj1.Problem1(t);
+Problem1(t);
 System.out.println();
 System.out.println("Problem -2");
 
@@ -57,7 +53,7 @@ for (int i = 0; i < n1; i++) {
 
 }
 
-System.out.println("Output (M1):"+obj2.Problem2(a));
+System.out.println("Output (M1):"+Problem2(a));
 System.out.println("Problem -3");
 System.out.print("Enter size of integer array:");
 int n31 = sc.nextInt();
@@ -76,7 +72,7 @@ c[2]=43;
 c[3]=55;
 c[4]=5;
 c[5]=10;*/
-System.out.println("Output (S):"+obj3.Problem3(a31));
+System.out.println("Output (S):"+Problem3(a31));
 System.out.println("Problem -4");
 System.out.println("Enter size of array");
 int g=sc.nextInt();
@@ -88,7 +84,7 @@ a1.add(sc.next());
 }
 System.out.println("Input (A1): "+a1);
 
-System.out.println("Output (A2):"+obj4.Problem4(a1));
+System.out.println("Output (A2):"+Problem4(a1));
 System.out.println("Problem -5");
 System.out.println("Enter size of arrayList");
 
@@ -105,7 +101,7 @@ a2.add("frameworks");
 a2.add("manoj");*/
 System.out.println("Input: "+a2);
 
-System.out.println("Output:"+obj5.Problem5(a2));
+System.out.println("Output:"+Problem5(a2));
 System.out.println("Problem -6");
 
 System.out.print("Enter size of String:- ");
@@ -119,9 +115,159 @@ for (int i = 0; i < n7; i++) {
 	a6.add(ele);
 
 }
-System.out.println("Output (M1): "+obj6.Problem6(a6));
+System.out.println("Output (M1): "+Problem6(a6));
 
 }
+	public static  void Problem1(String[] d) {
+		System.out.print("Input (S1): [");
+
+		Map<String, Integer> m = Strings(d);
+		int count = 0;
+		for (int i = 0; i < d.length; i++) {
+			count++;
+			if (count != d.length)
+				System.out.print("\"" + d[i] + "\",");
+			else
+				System.out.print("\"" + d[i] + "\"");
+		}
+		System.out.println("]");
+		System.out.print("Output: ");
+		count = 0;
+		for (Map.Entry<String, Integer> entry : m.entrySet()) {
+			count++;
+			if (count != m.size()) {
+				System.out.print(entry.getKey() + "-" + entry.getValue() + ", ");
+			} else {
+				System.out.print(entry.getKey() + "-" + entry.getValue());
+			}
+		}
+
+		
+	}
+	public static Map<String, Integer> Strings(String[] input) {
+		Map<String, Integer> m1 = new HashMap<String, Integer>();
+
+		for (int i = 0; i < input.length; i++) {
+			m1.put(input[i], m1.getOrDefault(input[i], 0) + 1);
+		}
+		return m1;
+
+	}
+	public static Map<String, String> Problem2(String[] a) {
+		ArrayList<String> b=new ArrayList<String>();
+		//Map<Character,String> m=new HashMap<Character,String>();
+		System.out.print("Input (S1): [");
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			count++;
+			if (count != a.length)
+				System.out.print("\"" + a[i] + "\",");
+			else
+				System.out.print("\"" + a[i] + "\"");
+		}
+		System.out.println("]");
+		Map<String, String> map = new HashMap<>();
+		for (int i = 0; i < a.length; i++) {
+			String s = a[i];
+			map.put(s.charAt(0) + "", map.getOrDefault(s.charAt(0) + "", "") + s);
+		}
+		
+			return map;
+	}
+	public static Set<Integer> Problem3(int[] st) {
+		System.out.println("Input (A1): " + Arrays.toString(st));
+		Set<Integer> set = new LinkedHashSet<>();
+		for (int i = 0; i < st.length; i++) {
+			int ele = st[i];
+			while (set.contains(ele)) {
+				ele++;
+			}
+			set.add(ele);
+		}
+	return set;
+	}
+	public static ArrayList<String> Problem4(ArrayList<String> a) {
+		ArrayList<String> a1=new ArrayList<String>();
+
+		ArrayList<Character> c=new ArrayList<Character>();
+		ArrayList<Character> b=new ArrayList<Character>();
+		int g=0;
+		String s="";
+		while(g<a.size()) {
+			
+		for(int i=0;i<a.get(g).length();i++) {
+			c.add(a.get(g).charAt(i));
+		}
+		//c.clear();
+		//System.out.println(c);
+		for(int i=0;i<a.get(g).length();i++) {
+			if(b.contains(c.get(i))) {
+				
+			}else {
+				b.add(c.get(i));
+			}
+			
+			}
+		//System.out.println(b);
+		for(int i=0;i<b.size();i++) {
+			s=s+b.get(i);
+		}
+		//System.out.println(s);
+		a1.add(s);
+		s="";
+		g++;
+		b.clear();
+		c.clear();
+			}
+
+
+		return a1;
+
+	}
+
+	public static Map<String,String> Problem5(ArrayList<String> a) {
+		Map<String,String> m=new HashMap<String,String>();
+
+		int g=0;
+		while(g<a.size()) {
+			
+
+		int l=a.get(g).length();
+			String sf=""+a.get(g).charAt(0);
+			String sl=""+a.get(g).charAt(l-1);
+
+				m.put(sf,sl);
+		g++;
+		}
+		//System.out.println(m);
+		return m;
+	}
+	public static Map<String,Boolean> Problem6(ArrayList<String> a6) {
+		System.out.print("Input (S1): [");
+		int count = 0;
+		for (int i = 0; i < a6.size(); i++) {
+			count++;
+			if (count != a6.size())
+				System.out.print("\"" + a6.get(i) + "\",");
+			else
+				System.out.print("\"" + a6.get(i) + "\"");
+		}
+		System.out.println("]");
+		Map<String, Boolean> map = new HashMap<>();
+		for (int i = 0; i < a6.size(); i++) {
+			if (map.containsKey(a6.get(i))) {
+				map.put(a6.get(i), true);
+			} else {
+				map.put(a6.get(i), false);
+			}
+		}
+		
+
+		return map;
+
+	}
+
+
 
 }
 
